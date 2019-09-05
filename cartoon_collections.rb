91268890c19e1c0fad2c40a94bf5b466b29aaec6
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-05 23:32:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index |dwarves, index|
-  puts "#{dwarves}"
+  puts "#{index +1}. #{dwarves}"
 end
 
 def summon_captain_planet# code an argument here
