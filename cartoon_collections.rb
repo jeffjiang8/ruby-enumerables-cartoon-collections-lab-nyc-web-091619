fbc3728bc2c3a77d60325d808fd67e39cbf731bb
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-05 23:39:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,22 @@
-def roll_call_dwarves(dwarves)# code an argument here
+def roll_call_dwarves (dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
   end
 end
 
-def summon_captain_planet(array)# code an argument here
+def summon_captain_planet (array)# code an argument here
   # Your code here
   array.map {|elements| elements.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls (array)# code an argument here
   # Your code here
+  if index.length > 4
+    return true 
+  else
+    return false 
+  end
 end
 
 def find_the_cheese# code an argument here
