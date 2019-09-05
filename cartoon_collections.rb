7a47903f6aa07c3f871b2e2a6d3c33711faf3f5d
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-05 23:45:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ end
 def find_the_cheese (array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  array.find do |cheese|
   array.include? (cheese_types)
 end
