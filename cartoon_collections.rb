196bcbda3e3d6bd718b17d276f040306603f35ea
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-05 23:28:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.length.times {|dwarves|
-  puts array[dwarves]}
+  array.each_with_index |array, 
 end
 
 def summon_captain_planet# code an argument here
