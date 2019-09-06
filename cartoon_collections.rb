b4a7e0f4ab5294b504ffb29962f2dceb44d60250
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-06 00:02:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,7 @@ def long_planeteer_calls (array)# code an argument here
   array.any? {|index| index.length > 4}
 end
 
+potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 def find_the_cheese(potentially_cheesy_items)
   cheeses = %w[gouda cheddar camembert]
 
